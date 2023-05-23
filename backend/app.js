af75bc675c41022bea28af1dc16d57207a809e2b
David Lima <david.lima17@gmail.com>
2023-05-23 18:41:21 -0500

    finished project

--- backend/app.js
+++ backend/app.js
@@ -11,29 +11,34 @@ const HttpError = require("./models/http-error.js");
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dxf7aoh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
+app.use(express.static(path.join("public")));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+//   next();
+// });
 
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
-  const error = new HttpError("Could not find this route.", 404);
-  throw error;
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
+
+// app.use((req, res, next) => {
+//   const error = new HttpError("Could not find this route.", 404);
+//   throw error;
+// });
 
 app.use((error, req, res, next) => {
   if (req.file) {
---
+++ backend/public/precache-manifest.89611b27f314965ebfbac9525704d20f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6825579c450e501acf6cd79cc1a38220",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05bf99e9627765243219",
+    "url": "/static/css/0.3df8cb10.chunk.css"
+  },
+  {
+    "revision": "fa7c7757dc73e5240804",
+    "url": "/static/css/4.55ab35b0.chunk.css"
+  },
+  {
+    "revision": "6bb154459f773621be6b",
+    "url": "/static/css/5.c09524ae.chunk.css"
+  },
+  {
+    "revision": "524f0be67ba4c6babdad",
+    "url": "/static/css/6.4c9fafef.chunk.css"
+  },
+  {
+    "revision": "dec0ef40fd053f8eccf4",
+    "url": "/static/css/7.38313bdd.chunk.css"
+  },
+  {
+    "revision": "5902c8ffaa35ba5b4fde",
+    "url": "/static/css/8.c540964c.chunk.css"
+  },
+  {
+    "revision": "77520b36287b399b9c01",
+    "url": "/static/css/main.8c937ad1.chunk.css"
+  },
+  {
+    "revision": "05bf99e9627765243219",
+    "url": "/static/js/0.bde6d76e.chunk.js"
+  },
+  {
+    "revision": "d18de8e1feb44794102f",
+    "url": "/static/js/3.30038642.chunk.js"
+  },
+  {
+    "revision": "fa7c7757dc73e5240804",
+    "url": "/static/js/4.4f3a0b4a.chunk.js"
+  },
+  {
+    "revision": "6bb154459f773621be6b",
+    "url": "/static/js/5.f08453f5.chunk.js"
+  },
+  {
+    "revision": "524f0be67ba4c6babdad",
+    "url": "/static/js/6.82cdde18.chunk.js"
+  },
+  {
+    "revision": "dec0ef40fd053f8eccf4",
+    "url": "/static/js/7.1735a713.chunk.js"
+  },
+  {
+    "revision": "5902c8ffaa35ba5b4fde",
+    "url": "/static/js/8.711bd3c2.chunk.js"
+  },
+  {
+    "revision": "77520b36287b399b9c01",
+    "url": "/static/js/main.a9c1d057.chunk.js"
+  },
+  {
+    "revision": "5212196d4be13b37a9bb",
+    "url": "/static/js/runtime-main.41c6fd1c.js"
+  }
+]);
